97816c0bfcb90e1a8b013d20462840f6e8e29b6e
ManojV007 <138686440+ManojV007@users.noreply.github.com>
2023-07-08 18:08:40 +0530

    added router to project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@
 import Header from "./components/Header"
 import "./App.css";
 import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About"
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
 
   return (
-    <>
-      <Header/>
-      <Home/>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
