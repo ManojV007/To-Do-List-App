6d95a8ec58082877d465d864601e74593e7bdc4f
ManojV007 <138686440+ManojV007@users.noreply.github.com>
2023-07-08 18:06:34 +0530

    added router to project

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Contact.css"
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <h1>Contact us on:<br></br><br></br>manojv.kmr10.mvk@gmail.com</h1>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import "./Header.css"
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <nav>
-      Get Ready to complete your Daily Tasks
-    </nav>
+    <>
+      <nav>
+        Get Ready to complete your Daily Tasks
+      </nav>
+      <div className="header">
+        <Link to='/'>HOME</Link>
+        <Link to='/about'>ABOUT</Link>
+        <Link to='/contact'>CONTACT</Link>
+      </div>
+    </>
   )
 }
 
